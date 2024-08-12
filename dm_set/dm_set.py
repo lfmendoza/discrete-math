9559b7b1614b87c18ed52f98ba29af6f816e2cee
Fernando Mendoza <lfmendoza.alvarez@gmail.com>
2024-08-12 16:37:12 -0600

    feat(dmset): performance improvements
    Refs: project-1

--- dm_set/dm_set.py
+++ dm_set/dm_set.py
@@ -1,82 +1,113 @@
 from .node import Node
 
 class DMSet:
+    """
+    Clase que representa un conjunto utilizando una estructura de datos basada en un set.
+    """
+
     def __init__(self):
-        self.head = None
+        """
+        Inicializa un conjunto vacío para almacenar los elementos.
+        """
+        self.elements = set()
 
     def add(self, value):
-        if not self.contains(value):
-            new_node = Node(value)
-            new_node.next = self.head
-            self.head = new_node
+        """
+        Añade un valor al conjunto si no existe ya en él.
+
+        :param value: El valor a añadir al conjunto.
+        :type value: any
+        """
+        self.elements.add(value)
 
     def remove(self, value):
-        current = self.head
-        previous = None
-        while current:
-            if current.value == value:
-                if previous:
-                    previous.next = current.next
-                else:
-                    self.head = current.next
-                return
-            previous = current
-            current = current.next
+        """
+        Elimina un valor del conjunto si existe.
+
+        :param value: El valor a eliminar del conjunto.
+        :type value: any
+        """
+        self.elements.discard(value)  # `discard` no arroja un error si el valor no está presente
 
     def contains(self, value):
-        current = self.head
-        while current:
-            if current.value == value:
-                return True
-            current = current.next
-        return False
+        """
+        Verifica si un valor está en el conjunto.
+
+        :param value: El valor a verificar en el conjunto.
+        :type value: any
+        :return: True si el valor existe en el conjunto, False en caso contrario.
+        :rtype: bool
+        """
+        return value in self.elements
 
     def union(self, other):
+        """
+        Retorna un nuevo conjunto que es la unión del conjunto actual y otro conjunto.
+
+        :param other: Otro conjunto con el que realizar la unión.
+        :type other: DMSet
+        :return: Un nuevo conjunto que contiene todos los elementos únicos de ambos conjuntos.
+        :rtype: DMSet
+        """
         result = DMSet()
-        current = self.head
-        while current:
-            result.add(current.value)
-            current = current.next
-        current = other.head
-        while current:
-            result.add(current.value)
-            current = current.next
+        result.elements = self.elements.union(other.elements)
         return result
 
     def intersection(self, other):
+        """
+        Retorna un nuevo conjunto que es la intersección del conjunto actual y otro conjunto.
+
+        :param other: Otro conjunto con el que realizar la intersección.
+        :type other: DMSet
+        :return: Un nuevo conjunto que contiene solo los elementos presentes en ambos conjuntos.
+        :rtype: DMSet
+        """
         result = DMSet()
-        current = self.head
-        while current:
-            if other.contains(current.value):
-                result.add(current.value)
-            current = current.next
+        result.elements = self.elements.intersection(other.elements)
         return result
 
     def complement(self, universal_set):
+        """
+        Retorna un nuevo conjunto que es el complemento del conjunto actual con respecto a un conjunto universal.
+
+        :param universal_set: El conjunto universal con el que comparar.
+        :type universal_set: DMSet
+        :return: Un nuevo conjunto que contiene elementos en el conjunto universal pero no en el conjunto actual.
+        :rtype: DMSet
+        """
         result = DMSet()
-        current = universal_set.head
-        while current:
-            if not self.contains(current.value):
-                result.add(current.value)
-            current = current.next
+        result.elements = universal_set.elements.difference(self.elements)
         return result
 
     def difference(self, other):
+        """
+        Retorna un nuevo conjunto que es la diferencia entre el conjunto actual y otro conjunto.
+
+        :param other: Otro conjunto que se va a restar del conjunto actual.
+        :type other: DMSet
+        :return: Un nuevo conjunto que contiene elementos en el conjunto actual pero no en el otro conjunto.
+        :rtype: DMSet
+        """
         result = DMSet()
-        current = self.head
-        while current:
-            if not other.contains(current.value):
-                result.add(current.value)
-            current = current.next
+        result.elements = self.elements.difference(other.elements)
         return result
 
     def symmetric_difference(self, other):
+        """
+        Retorna un nuevo conjunto que es la diferencia simétrica entre el conjunto actual y otro conjunto.
+
+        :param other: Otro conjunto con el que realizar la diferencia simétrica.
+        :type other: DMSet
+        :return: Un nuevo conjunto que contiene los elementos que están en cualquiera de los conjuntos pero no en ambos.
+        :rtype: DMSet
+        """
         return self.union(other).difference(self.intersection(other))
 
     def __str__(self):
-        values = []
-        current = self.head
-        while current:
-            values.append(current.value)
-            current = current.next
-        return "{" + ", ".join(map(str, values)) + "}"
+        """
+        Retorna una representación en cadena del conjunto.
+
+        :return: Una cadena en formato "{element1, element2, ...}" que representa el conjunto.
+        :rtype: str
+        """
+        return "{" + ", ".join(map(str, sorted(self.elements))) + "}"
